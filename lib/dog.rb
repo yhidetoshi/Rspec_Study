e9c843439e77ee617ce180c973e63a8de5edcf57
yhidetoshi <hyajima.1014@gmail.com>
2016-04-22 06:17:18 +0900

    add sinario

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,7 +1,9 @@
 class Dog
-  attr_accessor :name
+  attr_accessor :name, :fangs
 
   def initialize(name="Pochi")
      @name = name
+     @fangs = 2
   end
+
 end
--- spec/lib/dog_spec.rb
+++ spec/lib/dog_spec.rb
@@ -2,8 +2,15 @@ require "spec_helper"
 require "dog"
 
 describe "Dog" do
+  
   it "is named 'Pochi'" do
     dog = Dog.new
     expect(dog.name).to eq 'Pochi'
   end
+  
+  it "has fangs" do
+    dog = Dog.new
+    expect(dog.fangs).to eq 2
+  end
+
 end
